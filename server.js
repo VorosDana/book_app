6228bdc02032497ffaa89584c37ee49756dde732
HCoggers <harry.cogswell@gmail.com>
2019-02-27 10:53:41 -0800

    begins building out sql database and user bookshelf

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
+const pg = require('pg');
+
+require('dotenv').config();
 
 // Application Setup
 const app = express();
@@ -11,13 +14,21 @@ const PORT = process.env.PORT || 4000;
 // Application Middleware
 app.use(express.urlencoded({ extended: true }));
 
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
 // Set the file locations for ejs templates and static files like CSS
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
 // API Routes
+// Renders user's personal bookshelf
+app.get('/', getBookshelf);
+
 // Renders the search form
-app.get('/', newSearch);
+app.get('/form', newSearch);
 
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
@@ -32,20 +43,28 @@ function Book(info) {
   const placeHolder = 'https://i.imgur.com/J5LVHEL.jpg';
   if (info.imageLinks) {
     this.picture = info.imageLinks.smallThumbnail || placeHolder;
-    this.mouseover = info.imageLinks.thumbnail || placeHolder;
+    // this.mouseover = info.imageLinks.thumbnail || placeHolder;
   } else {
     this.picture = placeHolder;
-    this.mouseover = placeHolder;
+    // this.mouseover = placeHolder;
   }
-  this.title = info.title || 'No Title Avaialble';
+  this.title = info.title || 'No title available';
   this.authors = info.authors;
+  this.isbn = info.industryIdentifiers ? `${info.industryIdentifiers[0].type} ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
   this.description = info.description || 'No description available';
   this.link = info.infoLink;
+  // this.bookshelf = user input;
 }
 
 // Note that .ejs file extension is not required
+function getBookshelf(request, response) {
+  client.query(`SELECT * FROM books`)
+  .then(books => (response.render('pages/index', { searchResults: books.rows})));
+  app.use(express.static('./public'));
+}
+
 function newSearch(request, response) {
-  response.render('pages/index'); //location for ejs files
+  response.render('pages/form'); //location for ejs files
   app.use(express.static('./public'));//location for other files like css
 }
 
@@ -53,8 +72,8 @@ function newSearch(request, response) {
 function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-  console.log(request.body)
-  console.log(request.body.search)
+  console.log(request.body);
+  console.log(request.body.search);
 
   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
